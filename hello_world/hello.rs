dbbf08aa33a5076fe81bf0dd7f5994f1b326ee38
bl4ckf0xk <kavinduksks@gmail.com>
2025-02-10 21:21:41 +0530

    basics

--- hello_world/hello.rs
+++ hello_world/hello.rs
@@ -5,4 +5,12 @@
 fn main() {
     // print hello world
     println!("Hello, World!");
+
+    // defined 32bit value and 64bit floating value and 8bit value
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // destructed the tuple to x, y and z
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
 }
+
